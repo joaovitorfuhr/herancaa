5d08f7a22b23a6ea9ad4b9a89ab82faef970bcde
joaovitorfuhr <joao.fuhr@institutoivoti.org.br>
2017-10-10 19:33:53 -0300

    Criação do Cliente

---
+++ src/herancaa/Cliente.java
@@ -0,0 +1,14 @@
+package herancaa;  
+  
+public class Cliente extends Principal {  
+      
+    String cpf;  
+      
+    public void ImprimeNome (){  
+          
+        System.out.println("Nome do cliente é : " + nome + "\n Nº CPF: " + cpf + "\n Seu endereco :" + endereco);  
+          
+  
+    }  
+  
+}  
