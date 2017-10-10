a6825575cbc629b6118ea926172bf0a781d898d2
joaovitorfuhr <joao.fuhr@institutoivoti.org.br>
2017-10-10 19:32:17 -0300

    Criacao do vendedor

---
+++ src/herancaa/Vendedor.java
@@ -0,0 +1,15 @@
+Vendedor.java  
+  
+package heranca;  
+  
+Vendedor.java  
+  
+public class Vendedoredor extends Pessoa {  
+  
+      
+    String cnpj;  
+      
+    public void ImprimeNome (){  
+        System.out.println("O nome do fornecedor é : " + nome + "\n Cnpj: " + cnpj);  
+    }  
+}  
