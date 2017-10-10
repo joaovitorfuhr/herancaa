7c4526264145ae481740714c8a5b740de75f580a
joaovitorfuhr <joao.fuhr@institutoivoti.org.br>
2017-10-10 19:29:34 -0300

    Melhorando o principal

--- src/herancaa/Principal.java
+++ src/herancaa/Principal.java
@@ -1,5 +1,20 @@
-package herancaa;
+package heranca;  
+  
+Principal.java  
+public class Pessoa {  
+    String nome,  
+    idade,  
+    endereco= "R: Java ,10";
+    
+   
+    public void ImprimeNome(){  
+        System.out.println("o nome é:"carlos">;  
+        System.out.println("Endereco: " + endereco);  
+          
+          
+    }  
+      
+  
+}  
 
-public class Principal {
-
-}
+      
